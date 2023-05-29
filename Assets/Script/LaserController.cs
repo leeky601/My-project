15485fdf1dbbc53998e4fd258b5eb5246f71cd6e
leeky601 <48758887+leeky601@users.noreply.github.com>
2023-05-29 18:03:41 +0900

    레이저 투사체로 바꿈

---
+++ Assets/Script/LaserController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public float speed = 10f; // 레이저 이동 속도
+
+    private Vector3 direction; // 레이저 발사 방향
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public void SetDirection(Vector3 dir)
+    {
+        direction = dir;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 레이저와 충돌한 경우 추가 동작을 수행합니다.
+        // 충돌 처리 로직 추가
+        // ...
+
+        Destroy(gameObject); // 레이저 제거
+    }
+}
--- Assets/Script/SphereController.cs
+++ Assets/Script/SphereController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour
 {
+    public GameObject laserPrefab;
     private float timer; // 레이저 발사까지 남은 시간
     private Transform playerTransform; // 플레이어의 위치
     // Start is called before the first frame update
@@ -29,8 +30,14 @@ public class SphereController : MonoBehaviour
     {
         // 플레이어 위치로 레이저 발사
         Vector3 direction = (playerTransform.position - transform.position).normalized;
-        // 레이저 발사 로직 추가
-        // ...
+
+        // 레이저를 발사할 위치 계산 (구체의 현재 위치)
+        Vector3 laserSpawnPosition = transform.position;
+
+        // 레이저를 생성하고 발사합니다.
+        GameObject laser = Instantiate(laserPrefab, laserSpawnPosition, Quaternion.identity);
+        laser.GetComponent<LaserController>().SetDirection(direction);
+
 
         Destroy(gameObject); // 구체 제거
     }
